38e0002fcd0da6084f75907f978a2e8d24a5d819
akhilmohnn <akhilmohnn@outlook.com>
2024-10-02 01:09:05 +0530

    Largest among 3

--- PythonBasics/largest3.py
+++ PythonBasics/largest3.py
@@ -2,7 +2,9 @@ x=int(input("Enter first number"))
 y=int(input("Enter second number"))
 z=int(input("Enter third number"))
 
-if x>y:
+if x>y and x>z:
     print("1st is larger")
-else:
+elif y>x and y>z:
     print("2nd is larger")    
+else:
+    print("3rd number is larger")    
