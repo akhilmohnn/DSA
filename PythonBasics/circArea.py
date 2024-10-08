1a1d7ddc6e036c591b03c7087b26a2de0100f621
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2024-10-08 10:40:44 +0530

    To print radius of a circle

---
+++ PythonBasics/circArea.py
@@ -0,0 +1,16 @@
+"""DATE:03/10/24
+2.To print radius of a circle
+"""
+
+#PROGRAM:
+
+r=float(input("Enter the radius:"))
+
+pi=3.14
+
+area=pi*(r*r)
+
+print("Area of circle is:",area,"cm2")
+
+"""OUTPUT:Enter the radius:3
+          Area of circle is: 28.26 cm2 """
