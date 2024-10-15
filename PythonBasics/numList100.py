17d7928782c409eeb53cbfe8d0eb7e084851bb33
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2024-10-15 11:44:36 +0530

    Update numList100.py
    Prompt the user for a list of integers. For all values greater than 100,store 'over' instead

--- PythonBasics/numList100.py
+++ PythonBasics/numList100.py
@@ -1,10 +1,10 @@
 
-u=input("Enter a list of integers seperated by spaces")
+u=input("Enter a list of integers seperated by spaces") """stored as string initially"""
 numbers=u.split()
 result=[]
 
 for num in numbers:
-	num1 = int(num)
+	num1 = int(num)  """conerted into num"""
 	if num1>100:
 		result.append('over')
 	else:
