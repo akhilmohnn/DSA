923132b08be25d79fae08bba4e4a24f9e048d862
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2024-12-01 16:24:39 +0530

    To find the first missing positive integer
    Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.

---
+++ Arrays/firstMissingPositive.py
@@ -0,0 +1,14 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+
+        maxNum=max(nums)
+        minNum=min(nums)
+
+        if minNum<1:
+            minNum=1
+
+        for i in range(1,maxNum+1):
+            if i not in nums:
+                return i
+
+        return maxNum+1 
