8276eceb1df1c50f4c05facdda1935757f50f6b4
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2024-12-04 21:35:55 +0530

    To check string rotation possibility | Leetcode #796 | GFG POTD
    Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
    
    A shift on s consists of moving the leftmost character of s to the rightmost position.
    
    For example, if s = "abcde", then it will be "bcdea" after one shift.

---
+++ Strings/stringRotate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+
+        if len(s)!=len(goal):
+            return False
+
+        temp=s+s
+
+        if goal in temp:
+            return True
+
+        return False        
+        
