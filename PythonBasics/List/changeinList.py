58904ca5538be5a8a8622ee5a528b1f1603a6001
Akhil Mohanan <akhilmohnn@outlook.com>
2024-12-14 16:02:32 +0530

    Change/unique element in list1 from list2

---
+++ PythonBasics/List/changeinList.py
@@ -0,0 +1,20 @@
+n=int(input("Enter the number of colors to insert:"))
+
+color1=[]
+print("Enter the colors for list1")
+for x in range(n):
+    color=input()
+    color1.append(color)
+
+color2=[]
+print("Enter the colors for list1")
+for x in range(n):
+    color=input()
+    color2.append(color)
+
+unique=[]
+for x in color1:
+    if x not in color2:
+        unique.append(x)
+
+print("Unique colors are: ",unique)
