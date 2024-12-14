8e5c7d00d774789bf79a5b0b95ecbd16c8d9065c
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2024-12-14 13:29:00 +0530

    print over if >100

--- PythonBasics/C01-5b.py
+++ PythonBasics/C01-5b.py
@@ -1,4 +1,10 @@
 """Qn5.Prompt the user for a list of integers. For all values greater than 100,store 'over' instead , using list comprehent"""
 
-u=int(input("Enter a list of integers seperated by spaces")
 
+u=input("Enter a list of integers seperated by spaces")
+
+l=[int(x) for x in u.split()]
+
+result=['over' if x>100 else x for x in l]
+
+print(result)
